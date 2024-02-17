6b78d547fc5d7002fe5cbec70b5de274b006ff01
Aakash <pandit.aakash3@gmail.com>
2024-02-17 13:34:36 +0530

    Adding repos for JPA

---
+++ src/main/java/com/example/bookmyshow/api/BookMyShowController.java
@@ -0,0 +1,40 @@
+package com.example.bookmyshow.api;
+
+import com.example.bookmyshow.dto.ShowDTO;
+import com.example.bookmyshow.dto.ShowSeatDTO;
+import com.example.bookmyshow.service.BookMyShowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/bms")
+public class BookMyShowController {
+
+    private final BookMyShowService bookMyShowService;
+
+    @GetMapping(path = "/movieshows", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public List<ShowDTO> shows(@RequestBody ShowRequest showRequest){
+        return bookMyShowService.findAllShowsByMovieAndDateAndCity(showRequest.moviename, showRequest.date, showRequest.city);
+    }
+
+    @GetMapping(path = "/showseats", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public List<ShowSeatDTO> showSeats(@RequestBody ShowSeatRequest showSeatRequest){
+        return bookMyShowService.findAllAvailableSeatsForShow(showSeatRequest.getShowid());
+    }
+
+    @PostMapping(path = "/reserveseats", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String bookSeats(@RequestBody BookingRequest bookingRequest){
+        return bookMyShowService.reserveSeats(bookingRequest);
+    }
+
+    @PostMapping(path = "/confirmseats", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String conSeats(@RequestBody BookingRequest bookingRequest){
+        return bookMyShowService.confirmSeats(bookingRequest);
+    }
+
+}
