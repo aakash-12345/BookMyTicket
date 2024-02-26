df81b81b36ed38094e67f432b72326eaa59afcf0
Aakash <pandit.aakash3@gmail.com>
2024-02-26 20:11:40 +0530

    Added test cases

--- src/test/java/com/example/bookmyticket/scheduler/RemoveExpiredReservationsTest.java
+++ src/test/java/com/example/bookmyticket/scheduler/RemoveExpiredReservationsTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -36,18 +35,9 @@ public class RemoveExpiredReservationsTest {
         MockitoAnnotations.initMocks(this);
     }
 
-    @Value("${payment.session.timeout}")
-    private Integer sessionTimeout = 300000;
-
-    // Assuming getSessionTimeout() is a method in the same class
-    private long getSessionTimeout() {
-        // Implement getSessionTimeout() as per your requirement
-        return this.sessionTimeout; // Example session timeout in milliseconds
-    }
-
     @Test
     public void testRemoveExpiredReservations() {
-        // Create some pending show seats for testing
+
         ShowSeat pendingShowSeat1 = ShowSeat.builder()
                 .showSeatId(1L)
                 .showId(1L)
@@ -55,7 +45,7 @@ public class RemoveExpiredReservationsTest {
                 .reservationTime(LocalDateTime.now())
                 .bookingId(1L)
                 .status(ShowSeat.BookingStatus.RESERVED_PAYMENT_PENDING)
-                .build(); // Create pending show seat 1
+                .build();
         ShowSeat pendingShowSeat2 = ShowSeat.builder()
                 .showSeatId(2L)
                 .showId(1L)
@@ -63,34 +53,22 @@ public class RemoveExpiredReservationsTest {
                 .reservationTime(LocalDateTime.now().minusMinutes(10))
                 .bookingId(1L)
                 .status(ShowSeat.BookingStatus.RESERVED_PAYMENT_PENDING)
-                .build(); // Create pending show seat 2
+                .build();
         List<ShowSeat> pendingShowSeats = new ArrayList<>();
         pendingShowSeats.add(pendingShowSeat1);
         pendingShowSeats.add(pendingShowSeat2);
 
-        // Mock the behavior of showSeatRepository
         when(showSeatRepository.findAllByStatus(ShowSeat.BookingStatus.RESERVED_PAYMENT_PENDING))
                 .thenReturn(pendingShowSeats);
         Booking booking = new Booking();
         when(bookingRepository.findById(1L)).thenReturn(Optional.of(booking));
 
-        // Mock the behavior of bookingRepository
-        // ... (if needed)
-
-        // Mock the behavior of getSessionTimeout()
-//        when(removeExpiredReservationsObject.getSessionTimeout()).thenReturn(sessionTimeout); // Example session timeout for testing
-
-        // Call the method to be tested
         removeExpiredReservationsObject.removeExpiredReservations();
 
-        // Verify the expected behavior, for example:
-        // Verify that save method is called on showSeatRepository for each expired show seat
         verify(showSeatRepository, times(1)).save(any());
-        // Verify that deleteAll method is called on bookingRepository with the expected list of bookings
+
         verify(bookingRepository).deleteAll(anyList());
 
-        // Add assertions as per your specific requirements
-        // For example, assert that the show seat status is updated correctly
         assertEquals(ShowSeat.BookingStatus.RESERVED_PAYMENT_PENDING, pendingShowSeat1.getStatus());
         assertEquals(ShowSeat.BookingStatus.UNRESERVED, pendingShowSeat2.getStatus());
     }
